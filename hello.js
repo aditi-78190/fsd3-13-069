63b58e1329095b3135841d4a9619b41399cdc791
aditi-78190 <aditi.25b01010647@abes.ac.in>
2026-07-21 14:48:07 +0530

    commit and push

---
+++ hello.js
@@ -0,0 +1,15 @@
+function sayHello(name){
+    console.log(`Welcome to ${name}`);
+}
+
+const sayHi = (name) => {
+    console.log(`Welcome to ${name}`);
+};
+
+function sum(a,b){
+    return a+b;
+}
+
+sayHello("Aditi Gupta");
+console.log(`sum of 2 and 5 is $ {sum (2,5)}`);
+sayHi("Adii");
