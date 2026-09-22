ef584bb8f5049ecf0fb47cbff1af9eb6adeab167
aditi-78190 <aditi.25b01010647@abes.ac.in>
2026-09-22 14:16:55 +0530

    get all products commit

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -1,36 +1,99 @@
 import http from "http";
+import { getAllProducts } from "./products.js";
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/" && req.method === "GET") {
-    res.statusCode = 200;
-    res.end("GET Request");
-  } else if (req.url === "/" && req.method === "POST") {
-    //console.log("Request:", req);
-    let body =''
-    req.on('data',(chunk)=>{
-        body+= chunk;
-    });
-    req.on("end", ()=>{
-        const product = JSON.parse(body);
-        console.log("recieved product:", product);
-          res.statusCode = 201;
-          res.end(JSON.stringify({ msg: "product added", product }));
-    });
-  
 
-  } else if (req.url.startsWith("/products/") && req.method === "PUT") {
-    const productID = req.url.split('/').pop();
-    console.log('update product id:',productID);
+  // GET Products
+  if (req.url === "/api/v1/products" && req.method === "GET") {
+
     res.statusCode = 200;
-    res.end("PUT Request");
-    
-  } else if (req.url === "/" && req.method === "DELETE") {
+    res.setHeader("Content-Type", "application/json");
+
+    const data = getAllProducts();
+
+    res.end(JSON.stringify({
+      count: data.length,
+      data: data
+    }));
+
+  }
+
+  // POST Product
+  else if (req.url === "/" && req.method === "POST") {
+
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+
+    req.on("end", () => {
+
+      const product = JSON.parse(body);
+
+      console.log("Received product:", product);
+
+      res.statusCode = 201;
+      res.setHeader("Content-Type", "application/json");
+
+      res.end(JSON.stringify({
+        msg: "product added",
+        product: product
+      }));
+    });
+
+  }
+
+  // PUT Product
+  else if (
+    req.url.startsWith("/products/") &&
+    req.method === "PUT"
+  ) {
+
+    const productID = req.url.split("/").pop();
+
+    console.log("Update Product id:", productID);
+
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+
+    req.on("end", () => {
+
+      const product = JSON.parse(body);
+
+      product.id = productID;
+
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+
+      res.end(JSON.stringify({
+        msg: "product updated",
+        product: product
+      }));
+    });
+
+  }
+
+  // DELETE Product
+  else if (req.url === "/api/v1/products" && req.method === "DELETE") {
+
     res.statusCode = 200;
     res.end("DELETE Request");
-  } else {
-    res.statusCode = 404;
-    res.end("request not found");
+
   }
+
+  // Not Found
+  else {
+
+    res.statusCode = 404;
+    res.end("Request not found");
+  }
+
 });
 
-server.listen(5000, () => console.log("prg6 is running"));
+server.listen(5000, () => {
+  console.log("prg6 is running on port 5000");
+});
---
+++ lab3/products.js
@@ -0,0 +1,24 @@
+const products = [
+    {   
+        id:1 , 
+        name: 'Marker', 
+        qty: 100, 
+        price: 15
+    },
+    {   
+        id:2 , 
+        name: 'Duster', 
+        qty: 50, 
+        price: 20
+    },
+]
+let nextId = 3;
+
+export const getAllProducts =() =>{         //arrow function
+    return products;
+
+}
+
+export const addProducts = () =>{
+    
+}
